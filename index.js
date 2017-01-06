78a148b32307f0d51d46ff2edfff3cb4d0bcef60
shemoo <alshimaa.adel11@gmail.com>
2017-01-06 07:42:12 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,128 @@
 var IQexamObj = {
     name: "C Exam",
     time: 30,
+    maxQuesNum: 30,
     Quests: [
+        {
+            head: "The major language of World Wide Web is ___________?",
+            answers: [/*"a: HTML", "b: PHP", "c: ASP.NET", "d: Java"*/"True", "false"],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "HTML is an abbreviation for?",
+            answers: ["a: HiTech Meaningful Language", "b: HyperText Meaningful Language", "c: HiTech Markup Language", "d: HyperText Markup Language"],
+            correctAnswer: 3,
+            grade: 1
+        },
+        {
+            head: "___________ is a device from where the information is sent?",
+            answers: ["a: Transmitter", "b: Spreadsheet", "c: Simulation", "d: Modulation"],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "LAN is an abbreviation for ?",
+            answers: ["a: Large Access Network", "b: Local Access Network", "c: Large Area Network", "d: Local Area Network"],
+            correctAnswer: 3,
+            grade: 1
+        },
+        {
+            head: "Full form of URL is ?",
+            answers: ["a. Uniform Resource Locator", "b. Uniform Resource Link", "c. Uniform Registered Link", "d. Unified Resource Link"],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "FTP is an abbreviation for?",
+            answers: ["a: File Transfer Position", "b: File Transfer Protection", "c: File Transfer Protocol", "d: File Transfer Possibility"],
+            correctAnswer: 2,
+            grade: 1
+        },
+        {
+            head: "In which of the following form, data is stored in computer ?",
+            answers: ["a: Decimal", "b: Binary", "c: HexaDecimal", "d: Octal"],
+            correctAnswer: 1,
+            grade: 1
+        },
+        {
+            head: "The brain of any computer system is ?",
+            answers: ["a: ALU", "b: Memory", "c: CPU", "d: Control unit"],
+            correctAnswer: 2,
+            grade: 1
+        },
+        {
+            head: "Which of the following computer language is used for artificial intelligence ?",
+            answers: ["a: FORTRAN", "b: PROLOG", "c: C", "d: COBOL"],
+            correctAnswer: 1,
+            grade: 1
+        },
+        {
+            head: "The binary system uses powers of ?",
+            answers: ["a: 2", "b: 8", "c: 10", "d: 16"],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "The major language of World Wide Web is ___________?",
+            answers: [/*"a: HTML", "b: PHP", "c: ASP.NET", "d: Java"*/"True", "false"],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "HTML is an abbreviation for?",
+            answers: ["a: HiTech Meaningful Language", "b: HyperText Meaningful Language", "c: HiTech Markup Language", "d: HyperText Markup Language"],
+            correctAnswer: 3,
+            grade: 1
+        },
+        {
+            head: "___________ is a device from where the information is sent?",
+            answers: ["a: Transmitter", "b: Spreadsheet", "c: Simulation", "d: Modulation"],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "LAN is an abbreviation for ?",
+            answers: ["a: Large Access Network", "b: Local Access Network", "c: Large Area Network", "d: Local Area Network"],
+            correctAnswer: 3,
+            grade: 1
+        },
+        {
+            head: "Full form of URL is ?",
+            answers: ["a. Uniform Resource Locator", "b. Uniform Resource Link", "c. Uniform Registered Link", "d. Unified Resource Link"],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "FTP is an abbreviation for?",
+            answers: ["a: File Transfer Position", "b: File Transfer Protection", "c: File Transfer Protocol", "d: File Transfer Possibility"],
+            correctAnswer: 2,
+            grade: 1
+        },
+        {
+            head: "In which of the following form, data is stored in computer ?",
+            answers: ["a: Decimal", "b: Binary", "c: HexaDecimal", "d: Octal"],
+            correctAnswer: 1,
+            grade: 1
+        },
+        {
+            head: "The brain of any computer system is ?",
+            answers: ["a: ALU", "b: Memory", "c: CPU", "d: Control unit"],
+            correctAnswer: 2,
+            grade: 1
+        },
+        {
+            head: "Which of the following computer language is used for artificial intelligence ?",
+            answers: ["a: FORTRAN", "b: PROLOG", "c: C", "d: COBOL"],
+            correctAnswer: 1,
+            grade: 1
+        },
+        {
+            head: "The binary system uses powers of ?",
+            answers: ["a: 2", "b: 8", "c: 10", "d: 16"],
+            correctAnswer: 0,
+            grade: 1
+        },
         {
             head: "The major language of World Wide Web is ___________?",
             answers: [/*"a: HTML", "b: PHP", "c: ASP.NET", "d: Java"*/"True", "false"],
@@ -65,35 +186,224 @@ var IQexamObj = {
     ]
 };
 
+var EnglishExamObj = {
+    name: "English Exam",
+    time: 30,
+    maxQuesNum: 20,
+    Quests: [
+        {
+            head:"Look at this series: 1.5, 2.3, 3.1, 3.9, . . . What number should come next?",
+            answers: ["a. 4.2","b. 4.4","c. 4.7","d. 5.1"],
+            correctAnswer: 2,
+            grade: 1
+        },
+        {
+            head: "Look at this series: 5.2, 4.8, 4.4, 4, . . . What number should come next?",
+            answers: ["a. 3","b. 3.3","c. 3.5","d. 3.6"],
+            correctAnswer: 3,
+            grade: 1
+        },
+        {
+            head: "Look at this series: 2, 1, 12, 14, . . . What number should come next?",
+            answers: ["a. 1/3","b. 1/8 ","c. 2/8","d. 1/16 "],
+            correctAnswer: 1,
+            grade: 1
+        },
+        {
+            head: "28 25 5 21 18 5 14 ,...  What numbers should come next?",
+            answers: ["a. 11 5","b. 10 7","c. 11 8","d. 5 10","e. 10 5"],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "Look at this series: U32, V29, __, X23, Y20, . . . What number should fill the blank?",
+            answers: ["a. W26","b. W17","c. Z17","d. Z26"],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "Look at this series: XXIV, XX, __, XII, VIII, . . . What number should fill the blank?",
+            answers: ["a. XXII","b. XIII","c. XVI","d. IV"],
+            correctAnswer: 2,
+            grade: 1
+        },
+        {
+            head: "QAR RAS SAT TAU _____",
+            answers: ["a. UAV","b. UAT","c. TAS","d. TAT"],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "Which word does NOT belong with the others?",
+            answers: ["a. tulip","b. rose","c. bud","d. daisy"],
+            correctAnswer: 2,
+            grade: 1
+        },
+        {
+            head: "Which word does NOT belong with the others?",
+            answers: ["a. parsley","b. basil","c. dill","d. mayonnaise"],
+            correctAnswer: 3,
+            grade: 1
+        },
+        {
+            head: "Which word does NOT belong with the others?",
+            answers: ["a. book","b. index","c. glossary","d. chapter"],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "Which word does NOT belong with the others?",
+            answers: ["a. defendant","b. prosecutor","c. trial","d. judge"],
+            correctAnswer: 2,
+            grade: 1
+        },
+        {
+            head:"find the word that names a necessary part of book",
+            answers: ["a. fiction","b. pages","c. pictures","d. learning"],
+            correctAnswer: 1,
+            grade: 1
+        },
+        {
+            head: "find the word that names a necessary part of diploma",
+            answers: ["a. principal","b. curriculum","c. employment","d. graduation"],
+            correctAnswer: 3,
+            grade: 1
+        },
+        {
+            head: "find the word that names a necessary part of swimming",
+            answers: ["a. pool","b. bathing suit","c. water","d. life jacket "],
+            correctAnswer: 2,
+            grade: 1
+        },
+        {
+            head: "find the word that names a necessary part of desert",
+            answers: ["a. cactus","b. arid","c. oasis","d. flat"],
+            correctAnswer: 1,
+            grade: 1
+        },
+        {
+            head: "find the word that names a necessary part of itinerary",
+            answers: ["a. map","b. route","c. travel","d. guidebook"],
+            correctAnswer: 1,
+            grade: 1
+        },
+        {
+            head: "Window is to pane as book is to",
+            answers: ["a. novel.","b. glass.","c. cover.","d. page."],
+            correctAnswer: 3,
+            grade: 1
+        },
+        {
+            head: "Artist is to painting as senator is to",
+            answers: ["a. attorney.","b. law.","c. politician.","d. constituents."],
+            correctAnswer: 1,
+            grade: 1
+        },
+        {
+            head: "Guide is to direct as reduce is to",
+            answers: ["a. decrease.","b. maintain.","c. increase.","d. preserve."],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "Sponge is to porous as rubber is to",
+            answers: ["a. massive.","b. solid.","c. elastic.","d. inflexible."],
+            correctAnswer: 2,
+            grade: 1
+        },
+        {
+            head:"find the word that completes row of words.\n ant    fly     bee \n hamster squirrel _______",
+            answers: ["a. spider","b. mouse","c. rodent","d. cat"],
+            correctAnswer: 3,
+            grade: 1
+        },
+        {
+            head: "find the word that completes row of words.\npalette easel brush\ntextbook lesson plan _______",
+            answers: ["a. artist","b. teacher","c. report card","d. paint"],
+            correctAnswer: 1,
+            grade: 1
+        },
+        {//251
+            head: "find the word that completes row of words.\napples fruit supermarket\nnovel book _______",
+            answers: ["a. bookstore","b. magazine","c. vegetable","d. shopping "],
+            correctAnswer: 1,
+            grade: 1
+        },
+        {
+            head: "find the word that completes row of words.\nwalk skip run\ntoss pitch _______",
+            answers: ["a. swerve","b. hurl","c. jump","d. dance"],
+            correctAnswer: 3,
+            grade: 1
+        },
+        {
+            head: "find the word that completes row of words.\ndaisy flower plant\nbungalow house _______",
+            answers: ["a. building","b. cottage","c. apartment","d. city"],
+            correctAnswer: 3,
+            grade: 1
+        },
+        {
+            head: "find the pair that has a similar relationship.\n PETAL : FLOWER",
+            answers: ["a. salt : pepper","b. tire : bicycle","c. base : ball","d. sandals : shoes","e. puppy : dog"],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "find the pair that has a similar relationship.\nPULSATE : THROB",
+            answers: ["a. walk : run","b. tired : sleep","c. examine : scrutinize","d. ballet : dancer","e. find : lose"],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "find the pair that has a similar relationship.\nDEPRESSED : SAD",
+            answers: ["a. neat : considerate","b. towering : cringing","c. rapid : plodding","d. progressive : regressive","e. exhausted : tired"],
+            correctAnswer: 0,
+            grade: 1
+        },
+        {
+            head: "find the pair that has a similar relationship.\nCOTTON : BALE",
+            answers: ["a. butter : churn","b. wine : ferment","c. grain : shock","d. curd : cheese","e. beef : steak"],
+            correctAnswer: 1,
+            grade: 1
+        },
+        {
+            head: "find the pair that has a similar relationship.\nFINCH : BIRD",
+            answers: ["a. frog : toad","b. elephant : reptile","c. Dalmatian : dog","d. collie : marsupial","e. ant : ladybug"],
+            correctAnswer: 3,
+            grade: 1
+        }
+    ]
+};
+
 window.addEventListener("load", function () {
-    var questHead = document.createElement("h3");
-    var questNumHead = document.createElement("h2");
-    //Must be Dynamic
-    var AnsRadArr = [document.createElement("input"), document.createElement("input"), document.createElement("input"), document.createElement("input")];
-    var AnsLabelArr = [document.createElement("label"), document.createElement("label"), document.createElement("label"), document.createElement("label")];
+
     //to load fist element
+    mainDiv.innerHTML="";
     var index = 0;
-    appendQuest(index);
+    appendQuest(IQexamObj,index);
 
     btnStart.addEventListener("click", function () {
+        mainDiv.innerHTML="";
         index = 0;
-        appendQuest(index);
+        appendQuest(IQexamObj,index);
     });
     btnNext.addEventListener("click", function () {
         if (index >= 0 && index < IQexamObj.Quests.length - 1) {
+            mainDiv.innerHTML="";
             index = ++index;
-            appendQuest(index);
+            appendQuest(IQexamObj,index);
         }
     });
     btnPrev.addEventListener("click", function () {
         if (index > 0 && index < IQexamObj.Quests.length) {
+            mainDiv.innerHTML="";
             index = --index;
-            appendQuest(index);
+            appendQuest(IQexamObj,index);
         }
     });
     btnLast.addEventListener("click", function () {
+        mainDiv.innerHTML="";
         index = IQexamObj.Quests.length - 1;
-        appendQuest(index);
+        appendQuest(IQexamObj,index);
     });
     btnMark.addEventListener("click", function () {
          appendSideQuest(index);
@@ -112,34 +422,40 @@ window.addEventListener("load", function () {
     });
 
     //function to append elements in the mainDiv
-    function appendQuest(index)
+    function appendQuest(obj,index)
     {
+        var questNumHead = document.createElement("h2");
         questNumHead.innerText = "Question No." + (index + 1);
         mainDiv.appendChild(questNumHead);
-        var currentQues = IQexamObj.Quests[index];
+        var currentQues = obj.Quests[index];
 
+        var questHead = document.createElement("h3");
         questHead.innerText = currentQues.head;
         mainDiv.appendChild(questHead);
 
         for (var x = 0; x < currentQues.answers.length; x++)
         {
-            // var rad = document.createElement("input");
-            AnsRadArr[x].setAttribute("type", "radio");
-            AnsRadArr[x].setAttribute("value", x);
-            AnsRadArr[x].setAttribute("name", "q" + index);
-            AnsRadArr[x].checked=false;
-            AnsRadArr[x].addEventListener("click", function () {
+            var radBtn = document.createElement("input");
+            radBtn.setAttribute("type", "radio");
+            radBtn.setAttribute("value", x);
+            radBtn.setAttribute("name", "q" + index);
+            radBtn.addEventListener("click", function () {
                 qIndex = Number(this.name.substring(1));
-                IQexamObj.Quests[qIndex].userAnswer = this.value;
+                obj.Quests[qIndex].userAnswer = this.value;
             });
-            if (currentQues.userAnswer != undefined)
-                AnsRadArr[currentQues.userAnswer].checked=true;
 
-            mainDiv.appendChild(AnsRadArr[x]);
+            mainDiv.appendChild(radBtn);
 
-            //var lable = document.createElement("lable");
-            AnsLabelArr[x].innerText = currentQues.answers[x];
-            mainDiv.appendChild(AnsLabelArr[x]);
+            var lable = document.createElement("lable");
+            lable.innerText = currentQues.answers[x];
+            if(obj.Quests.userAnswer != "")
+            {
+                if(index == obj.Quests[x].userAnswer)
+                {
+                    radBtn.checked = true;
+                }
+            }
+            mainDiv.appendChild(lable);
         };
     };
     var MarkQuestArr=[];   // when we click mark empty Array so must be global
@@ -155,8 +471,9 @@ window.addEventListener("load", function () {
                 sideDiv.appendChild(qSideHead);
             }
         qSideHead.addEventListener("click",function(){
+            mainDiv.innerHTML="";
             index=Markindex;
-            appendQuest(Markindex);
+            appendQuest(IQexamObj,Markindex);
         });
     };
     //Random Fuunction
